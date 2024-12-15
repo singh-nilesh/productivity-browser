afb8c3c12eb89e8743bc09de13564b77eecd0c7e
singh-nilesh <nileshsingh2021fybsc@gmail.com>
2024-12-15 17:13:02 +0530

    Revert "re-structured the project file"
    This reverts commit 89c4be9b005266d5b4c9e631e8d4c4f77b888d7c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,jsx,ts,tsx}",
-    "./src/screens/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
