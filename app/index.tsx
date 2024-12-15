85e70f1724ae7fef9abdf97d44e4a05bf702cd82
singh-nilesh <nileshsingh2021fybsc@gmail.com>
2024-12-15 08:47:59 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className=" justify-center items-center">
+      <Text className="text-4xl m-10">Productivity Browser </Text>
+    </View>
+  );
+}
